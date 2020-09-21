61c71233e4c5d9962df74e7f36786ffbe4f88120
tanaka.takurou <tanaka.takurou@icloud.com>
2020-09-22 00:59:26 +0900

    fix for aws-sdk-go-v2

--- api/main.go
+++ api/main.go
@@ -9,20 +9,21 @@ import (
 	"bytes"
 	"errors"
 	"strings"
-	"strconv"
 	"context"
 	"encoding/json"
 	"path/filepath"
 	"encoding/base64"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
 )
 
 type UserResponse struct {
@@ -45,8 +46,12 @@ type ImgData struct {
 
 type Response events.APIGatewayProxyResponse
 
-const layout       string = "2006-01-02 15:04"
-const layout2      string = "20060102150405"
+var cfg aws.Config
+var dynamodbClient *dynamodb.Client
+var cognitoClient *cognitoidentityprovider.Client
+
+const layout  string = "2006-01-02 15:04"
+const layout2 string = "20060102150405"
 
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
 	var jsonBytes []byte
@@ -58,7 +63,7 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 		case "login" :
 			if n, ok := d["name"]; ok {
 				if p, ok := d["pass"]; ok {
-					t, e := login(n, p)
+					t, e := login(ctx, n, p)
 					if e == nil {
 						jsonBytes, _ = json.Marshal(UserResponse{Name: n, Token: t, ImgUrl: ""})
 					} else {
@@ -68,7 +73,7 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 			}
 		case "getuser" :
 			if t, ok := d["token"]; ok {
-				n, e := getuser(t)
+				n, e := getuser(ctx, t)
 				if e == nil {
 					jsonBytes, _ = json.Marshal(UserResponse{Name: n, Token: "", ImgUrl: ""})
 				} else {
@@ -79,14 +84,14 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 			if t, ok := d["token"]; ok {
 				if p, ok := d["pass"]; ok {
 					if np, ok := d["newpass"]; ok {
-						err = changePass(t, p, np)
+						err = changePass(ctx, t, p, np)
 						jsonBytes, _ = json.Marshal(UserResponse{Name: "", Token: "", ImgUrl: ""})
 					}
 				}
 			}
 		case "logout" :
 			if t, ok := d["token"]; ok {
-				err = logout(t)
+				err = logout(ctx, t)
 				if err == nil {
 					jsonBytes, _ = json.Marshal(UserResponse{Name: "ok", Token: "Logout", ImgUrl: ""})
 				}
@@ -95,7 +100,7 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 			if n, ok := d["name"]; ok {
 				if p, ok := d["pass"]; ok {
 					if m, ok := d["mail"]; ok {
-						err = signup(n, p, m)
+						err = signup(ctx, n, p, m)
 						jsonBytes, _ = json.Marshal(UserResponse{Name: n, Token: "", ImgUrl: ""})
 					}
 				}
@@ -103,15 +108,15 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 		case "confirmsignup" :
 			if n, ok := d["name"]; ok {
 				if c, ok := d["code"]; ok {
-					err = confirmSignup(n, c)
+					err = confirmSignup(ctx, n, c)
 					jsonBytes, _ = json.Marshal(UserResponse{Name: n, Token: "", ImgUrl: ""})
 				}
 			}
 		case "getimg" :
 			if t, ok := d["token"]; ok {
-				n, e := getuser(t)
+				n, e := getuser(ctx, t)
 				if e == nil {
-					imgUrl, _ := getImage(os.Getenv("IMG_TABLE_NAME"), n)
+					imgUrl, _ := getImage(ctx, os.Getenv("IMG_TABLE_NAME"), n)
 					jsonBytes, _ = json.Marshal(UserResponse{Name: n, Token: "", ImgUrl: imgUrl})
 				} else {
 					err = e
@@ -119,11 +124,11 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 			}
 		case "uploadimg" :
 			if t, ok := d["token"]; ok {
-				n, e := getuser(t)
+				n, e := getuser(ctx, t)
 				if e == nil {
 					if v, ok := d["filename"]; ok {
 						if w, ok := d["filedata"]; ok {
-							imgUrl, _ := uploadImage(os.Getenv("IMG_TABLE_NAME"), os.Getenv("BUCKET_NAME"), v, w, n)
+							imgUrl, _ := uploadImage(ctx, os.Getenv("IMG_TABLE_NAME"), os.Getenv("BUCKET_NAME"), v, w, n)
 							jsonBytes, _ = json.Marshal(UserResponse{Name: n, Token: "", ImgUrl: imgUrl})
 						}
 					}
@@ -151,195 +156,176 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	}, nil
 }
 
-func login(name string, pass string)(string, error) {
-	svc := cognitoidentityprovider.New(session.New(), &aws.Config{
-		Region: aws.String("ap-northeast-1"),
-	})
+func login(ctx context.Context, name string, pass string)(string, error) {
+	if cognitoClient == nil {
+		cognitoClient = cognitoidentityprovider.New(cfg)
+	}
 
-	params := &cognitoidentityprovider.InitiateAuthInput{
-		AuthFlow: aws.String("USER_PASSWORD_AUTH"),
-		AuthParameters: map[string]*string{
-			"USERNAME": aws.String(name),
-			"PASSWORD": aws.String(pass),
+	input := &cognitoidentityprovider.InitiateAuthInput{
+		AuthFlow: cognitoidentityprovider.AuthFlowTypeUserPasswordAuth,
+		AuthParameters: map[string]string{
+			"USERNAME": name,
+			"PASSWORD": pass,
 		},
 		ClientId: aws.String(os.Getenv("CLIENT_ID")),
 	}
 
-	res, err := svc.InitiateAuth(params)
+	req := cognitoClient.InitiateAuthRequest(input)
+	res, err := req.Send(ctx)
 	if err != nil {
 		return "", err
 	}
-	return aws.StringValue(res.AuthenticationResult.AccessToken), nil
+	return aws.StringValue(res.InitiateAuthOutput.AuthenticationResult.AccessToken), nil
 }
 
-func getuser(token string)(string, error) {
-	svc := cognitoidentityprovider.New(session.New(), &aws.Config{
-		Region: aws.String("ap-northeast-1"),
-	})
+func getuser(ctx context.Context, token string)(string, error) {
+	if cognitoClient == nil {
+		cognitoClient = cognitoidentityprovider.New(cfg)
+	}
 
-	params := &cognitoidentityprovider.GetUserInput{
+	input := &cognitoidentityprovider.GetUserInput{
 		AccessToken: aws.String(token),
 	}
 
-	res, err := svc.GetUser(params)
+	req := cognitoClient.GetUserRequest(input)
+	res, err := req.Send(ctx)
 	if err != nil {
 		return "", err
 	}
-	return aws.StringValue(res.Username), nil
+	return aws.StringValue(res.GetUserOutput.Username), nil
 }
 
-func changePass(token string, pass string, newPass string) error {
-	svc := cognitoidentityprovider.New(session.New(), &aws.Config{
-		Region: aws.String("ap-northeast-1"),
-	})
+func changePass(ctx context.Context, token string, pass string, newPass string) error {
+	if cognitoClient == nil {
+		cognitoClient = cognitoidentityprovider.New(cfg)
+	}
 
-	params := &cognitoidentityprovider.ChangePasswordInput{
+	input := &cognitoidentityprovider.ChangePasswordInput{
 		AccessToken:      aws.String(token),
 		PreviousPassword: aws.String(pass),
 		ProposedPassword: aws.String(newPass),
 	}
 
-	_, err := svc.ChangePassword(params)
-	if err != nil {
-		return err
-	}
-	return nil
+	req := cognitoClient.ChangePasswordRequest(input)
+	_, err := req.Send(ctx)
+	return err
 }
 
-func logout(token string) error {
-	svc := cognitoidentityprovider.New(session.New(), &aws.Config{
-		Region: aws.String("ap-northeast-1"),
-	})
-	o_params := &cognitoidentityprovider.GlobalSignOutInput{
+func logout(ctx context.Context, token string) error {
+	if cognitoClient == nil {
+		cognitoClient = cognitoidentityprovider.New(cfg)
+	}
+
+	input := &cognitoidentityprovider.GlobalSignOutInput{
 		AccessToken: aws.String(token),
 	}
-	_, err := svc.GlobalSignOut(o_params)
-	if err != nil {
-		return err
-	}
-	return nil
+
+	req := cognitoClient.GlobalSignOutRequest(input)
+	_, err := req.Send(ctx)
+	return err
 }
 
-func signup(name string, pass string, mail string) error {
-	svc := cognitoidentityprovider.New(session.New(), &aws.Config{
-		Region: aws.String("ap-northeast-1"),
-	})
+func signup(ctx context.Context, name string, pass string, mail string) error {
+	if cognitoClient == nil {
+		cognitoClient = cognitoidentityprovider.New(cfg)
+	}
+
 	ua := &cognitoidentityprovider.AttributeType {
 		Name: aws.String("email"),
 		Value: aws.String(mail),
 	}
-	params := &cognitoidentityprovider.SignUpInput{
+	input := &cognitoidentityprovider.SignUpInput{
 		Username: aws.String(name),
 		Password: aws.String(pass),
 		ClientId: aws.String(os.Getenv("CLIENT_ID")),
-		UserAttributes: []*cognitoidentityprovider.AttributeType{
-			ua,
+		UserAttributes: []cognitoidentityprovider.AttributeType{
+			*ua,
 		},
 	}
 
-	_, err := svc.SignUp(params)
-	if err != nil {
-		return err
-	}
-	return nil
+	req := cognitoClient.SignUpRequest(input)
+	_, err := req.Send(ctx)
+	return err
 }
 
-func confirmSignup(name string, confirmationCode string) error {
-	svc := cognitoidentityprovider.New(session.New(), &aws.Config{
-		Region: aws.String("ap-northeast-1"),
-	})
+func confirmSignup(ctx context.Context, name string, confirmationCode string) error {
+	if cognitoClient == nil {
+		cognitoClient = cognitoidentityprovider.New(cfg)
+	}
 
-	params := &cognitoidentityprovider.ConfirmSignUpInput{
+	input := &cognitoidentityprovider.ConfirmSignUpInput{
 		Username: aws.String(name),
 		ConfirmationCode: aws.String(confirmationCode),
 		ClientId: aws.String(os.Getenv("CLIENT_ID")),
 	}
 
-	_, err := svc.ConfirmSignUp(params)
-	if err != nil {
-		return err
-	}
-	return nil
+	req := cognitoClient.ConfirmSignUpRequest(input)
+	_, err := req.Send(ctx)
+	return err
 }
 
-func scan(tableName string, filt expression.ConditionBuilder)(*dynamodb.ScanOutput, error)  {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	svc := dynamodb.New(sess)
+func scan(ctx context.Context, tableName string, filt expression.ConditionBuilder)(*dynamodb.ScanOutput, error)  {
+	if dynamodbClient == nil {
+		dynamodbClient = dynamodb.New(cfg)
+	}
 	expr, err := expression.NewBuilder().WithFilter(filt).Build()
 	if err != nil {
 		return nil, err
 	}
-	params := &dynamodb.ScanInput{
+	input := &dynamodb.ScanInput{
 		ExpressionAttributeNames:  expr.Names(),
 		ExpressionAttributeValues: expr.Values(),
 		FilterExpression:          expr.Filter(),
 		ProjectionExpression:      expr.Projection(),
 		TableName:                 aws.String(tableName),
 	}
-	return svc.Scan(params)
+	req := dynamodbClient.ScanRequest(input)
+	res, err := req.Send(ctx)
+	return res.ScanOutput, err
 }
 
-func put(tableName string, av map[string]*dynamodb.AttributeValue) error {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	svc := dynamodb.New(sess)
+func put(ctx context.Context, tableName string, av map[string]dynamodb.AttributeValue) error {
+	if dynamodbClient == nil {
+		dynamodbClient = dynamodb.New(cfg)
+	}
 	input := &dynamodb.PutItemInput{
 		Item:      av,
 		TableName: aws.String(tableName),
 	}
-	_, err := svc.PutItem(input)
+	req := dynamodbClient.PutItemRequest(input)
+	_, err := req.Send(ctx)
 	return err
 }
 
-func get(tableName string, key map[string]*dynamodb.AttributeValue, att string)(*dynamodb.GetItemOutput, error) {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	svc := dynamodb.New(sess)
-	input := &dynamodb.GetItemInput{
-		TableName: aws.String(tableName),
-		Key: key,
-		AttributesToGet: []*string{
-			aws.String(att),
-		},
-		ConsistentRead: aws.Bool(true),
-		ReturnConsumedCapacity: aws.String("NONE"),
+func update(ctx context.Context, tableName string, an map[string]string, av map[string]dynamodb.AttributeValue, key map[string]dynamodb.AttributeValue, updateExpression string) error {
+	if dynamodbClient == nil {
+		dynamodbClient = dynamodb.New(cfg)
 	}
-	return svc.GetItem(input)
-}
-
-func update(tableName string, an map[string]*string, av map[string]*dynamodb.AttributeValue, key map[string]*dynamodb.AttributeValue, updateExpression string) error {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	svc := dynamodb.New(sess)
 	input := &dynamodb.UpdateItemInput{
 		ExpressionAttributeNames: an,
 		ExpressionAttributeValues: av,
 		TableName: aws.String(tableName),
 		Key: key,
-		ReturnValues:     aws.String("UPDATED_NEW"),
+		ReturnValues:     dynamodb.ReturnValueUpdatedNew,
 		UpdateExpression: aws.String(updateExpression),
 	}
 
-	_, err := svc.UpdateItem(input)
+	req := dynamodbClient.UpdateItemRequest(input)
+	_, err := req.Send(ctx)
 	return err
 }
 
-func getImgCount(imgTableName string)(*int64, error)  {
-	result, err := scan(imgTableName, expression.NotEqual(expression.Name("status"), expression.Value(-1)))
+func getImgCount(ctx context.Context, imgTableName string)(*int64, error)  {
+	result, err := scan(ctx, imgTableName, expression.NotEqual(expression.Name("status"), expression.Value(-1)))
 	if err != nil {
 		return nil, err
 	}
 	return result.ScannedCount, nil
 }
 
-func putImg(imgTableName string, url string, name string) error {
+func putImg(ctx context.Context, imgTableName string, url string, name string) error {
 	t := time.Now()
-	count, err := getImgCount(imgTableName)
+	count, err := getImgCount(ctx, imgTableName)
 	if err != nil {
 		return err
 	}
@@ -354,55 +340,15 @@ func putImg(imgTableName string, url string, name string) error {
 	if err != nil {
 		return err
 	}
-	err = put(imgTableName, av)
+	err = put(ctx, imgTableName, av)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func updateImg(imgTableName string, img_id int, url string, updated string) error {
-	an := map[string]*string{
-		"#u": aws.String("url"),
-		"#d": aws.String("updated"),
-	}
-	av := map[string]*dynamodb.AttributeValue{
-		":u": {
-			S: aws.String(url),
-		},
-		":d": {
-			S: aws.String(updated),
-		},
-	}
-	key := map[string]*dynamodb.AttributeValue{
-		"img_id": {
-			N: aws.String(strconv.Itoa(img_id)),
-		},
-	}
-	updateExpression := "set #u = :u, #d = :d"
-
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	svc := dynamodb.New(sess)
-	input := &dynamodb.UpdateItemInput{
-		ExpressionAttributeNames: an,
-		ExpressionAttributeValues: av,
-		TableName: aws.String(imgTableName),
-		Key: key,
-		ReturnValues:     aws.String("UPDATED_NEW"),
-		UpdateExpression: aws.String(updateExpression),
-	}
-
-	_, err := svc.UpdateItem(input)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func getImage(imgTableName string, username string)(string, error) {
-	result, err := scan(imgTableName, expression.Name("name").Equal(expression.Value(username)))
+func getImage(ctx context.Context, imgTableName string, username string)(string, error) {
+	result, err := scan(ctx, imgTableName, expression.Name("name").Equal(expression.Value(username)))
 	if err != nil {
 		log.Print(err)
 		return "", err
@@ -425,7 +371,7 @@ func getImage(imgTableName string, username string)(string, error) {
 	return imgDataList[0].Url, nil
 }
 
-func uploadImage(imgTableName string, bucketName string, filename string, filedata string, username string)(string, error) {
+func uploadImage(ctx context.Context, imgTableName string, bucketName string, filename string, filedata string, username string)(string, error) {
 	t := time.Now()
 	b64data := filedata[strings.IndexByte(filedata, ',')+1:]
 	data, err := base64.StdEncoding.DecodeString(b64data)
@@ -448,17 +394,10 @@ func uploadImage(imgTableName string, bucketName string, filename string, fileda
 	default:
 		return "", errors.New("this extension is invalid")
 	}
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(os.Getenv("REGION"))},
-	)
-	if err != nil {
-		log.Print(err)
-		return "", err
-	}
 	filename_ := string([]rune(filename)[:(len(filename) - len(extension))]) + t.Format(layout2) + extension
-	uploader := s3manager.NewUploader(sess)
+	uploader := s3manager.NewUploader(cfg)
 	_, err = uploader.Upload(&s3manager.UploadInput{
-		ACL: aws.String("public-read"),
+		ACL: s3.ObjectCannedACLPublicRead,
 		Bucket: aws.String(bucketName),
 		Key: aws.String(filename_),
 		Body: bytes.NewReader(data),
@@ -469,8 +408,17 @@ func uploadImage(imgTableName string, bucketName string, filename string, fileda
 		return "", err
 	}
 	imgUrl := "https://" + bucketName + ".s3-" + os.Getenv("REGION") + ".amazonaws.com/" + filename_
-	putImg(imgTableName, imgUrl, username)
+	putImg(ctx, imgTableName, imgUrl, username)
 	return imgUrl, nil
+}
+
+func init() {
+	var err error
+	cfg, err = external.LoadDefaultAWSConfig()
+	cfg.Region = os.Getenv("REGION")
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 func main() {
